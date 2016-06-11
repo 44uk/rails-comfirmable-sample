473ae0ad7d0e166297f820dda6c165840d8db822
Yoshiyuki Ieyama <yukku0423@gmail.com>
2016-06-11 09:45:11 +0900

    separation of concerns

---
+++ app/models/concerns/confirmable.rb
@@ -0,0 +1,22 @@
+module Confirmable
+  extend ActiveSupport::Concern
+
+  included do
+    validates :confirmed, acceptance: true
+    validates :completed, acceptance: true
+
+    after_validation :confirming
+
+    private
+
+    def confirming
+      self.confirmed = errors.include?(:confirmed) && errors.size == 1 ? '1' : ''
+      clear_confirming_errors
+    end
+
+    def clear_confirming_errors
+      errors.delete :confirmed
+      errors.delete :completed
+    end
+  end
+end
