f805bcc7d11656401fe4a2f8844bb34b53493efd
Yoshiyuki Ieyama <yukku0423@gmail.com>
2016-06-11 09:22:01 +0900

    implement confirming

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,18 @@
 class User < ActiveRecord::Base
+  validates :confirmed, acceptance: true
+  validates :completed, acceptance: true
+
+  after_validation :confirming
+
+  private
+
+  def confirming
+    self.confirmed = errors.include?(:confirmed) && errors.size == 1 ? '1' : ''
+    clear_confirming_errors
+  end
+
+  def clear_confirming_errors
+    errors.delete :confirmed
+    errors.delete :completed
+  end
 end
