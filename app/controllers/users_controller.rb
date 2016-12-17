c2b0cf2231507a07bb83022ca06690e66ab727ca
Yoshiyuki Ieyama <yukku0423@gmail.com>
2016-12-17 15:13:24 +0900

    rename attributes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,6 +69,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:email, :password, :name, :confirmed, :completed)
+      params.require(:user).permit(:email, :password, :name, :submited, :confirmed)
     end
 end
--- app/models/concerns/confirmable.rb
+++ app/models/concerns/confirmable.rb
@@ -2,21 +2,23 @@ module Confirmable
   extend ActiveSupport::Concern
 
   included do
+    # new -> confirm
+    validates :submited, acceptance: true
+    # confirm -> create
     validates :confirmed, acceptance: true
-    validates :completed, acceptance: true
 
     after_validation :confirming
 
     private
 
     def confirming
-      self.confirmed = errors.include?(:confirmed) && errors.size == 1 ? '1' : ''
+      self.submited = errors.include?(:submited) && errors.size == 1 ? '1' : ''
       clear_confirming_errors
     end
 
     def clear_confirming_errors
+      errors.delete :submited
       errors.delete :confirmed
-      errors.delete :completed
     end
   end
 end
